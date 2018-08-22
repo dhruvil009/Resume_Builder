85b1d3779462619e102665f1f5aad5a47f96450d
Satya Patel <32892410+SatyaPatel5@users.noreply.github.com>
2018-08-23 00:36:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,6 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-keySkills = ['Java', 'C', 'Flask', 'Python', 'Swift']
-work_exp = [
-    {
-        'company':"Xyz Inc",
-        'duration':"May 2018 June 2018",
-        'description':"Batch of 2019 (Badi Galti)"
-    },
-    {
-        'company':"jhksjf University",
-        'duration':"May 2018 June 2018",
-        'description':"Batch of 2024 (Sabse Badi Galti)"
-    },
-{
-        'company':"jhksjf University",
-        'duration':"May 2018 June 2018",
-        'description':"Batch of 2024 (Sabse Badi Galti)"
-    },
-{
-        'company':"jhksjf University",
-        'duration':"May 2018 June 2018",
-        'description':"Batch of 2024 (Sabse Badi Galti)"
-    }
-]
-
-education = [
-    {
-        'school':"Nirma University",
-        'degree':"Computer",
-        'description':"Batch of 2019 (Badi Galti)"
-    },
-    {
-        'school':"jhksjf University",
-        'degree':"Computer",
-        'description':"Batch of 2024 (Sabse Badi Galti)"
-    }
-]
 
 @app.route('/')
 def hello_world():
@@ -45,11 +9,46 @@ def hello_world():
 @app.route('/View')
 def hello():
     return 'Welcome to Home Page!'
+
+
+## After clicking Submit
 @app.route('/go', methods=['POST'])
 def go():
     if(request.method == 'POST'):
-        name = 'satya'#request.form['name_0']
-        return render_template('Template1.html', name=name, keySkills = keySkills, education = education, work_exp = work_exp)
+        name = request.form['name']
+        email = request.form['email']
+        contact = request.form['contact']
+        profile = request.form['profile']
+
+
+        work_exp_count = request.form['work_exp_count']
+        work_exp = []
+        for i in range(int(work_exp_count)):
+            a = request.form['workexp_'+str(i+1)].split("\n")
+            work_exp.append({
+                'company':a[0],
+                'duration': a[1],
+                'description': a[2]
+            })
+
+        education_count = request.form['education_count']
+        education = []
+        for i in range(int(education_count)):
+            a = request.form['education_' + str(i + 1)].split("\n")
+            education.append({
+            'school':a[0],
+            'degree':a[1],
+            'description':a[2]
+        })
+
+        skills_count = request.form['skills_count']
+        keySkills = []
+        for i in range(int(skills_count)):
+            a = request.form['skills_' + str(i + 1)]
+            keySkills.append(a)
+        return render_template('index.html', name=name, email=email, contact=contact, profile=profile, keySkills = keySkills, education = education, work_exp = work_exp)
+
+## For Rendering Form
 @app.route('/form')
 def form():
     return render_template('getform.html')
